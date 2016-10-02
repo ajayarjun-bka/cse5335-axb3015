9ee00ff163561180069eeb38baa8d70255fc52a9
Ajay Arjun <arjun.bka@gmail.com>
2016-10-02 17:00:35 -0500

    express app

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-var http = require('http')
+var express = require('express');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.end("<h1>Hello World</h1>");
-}).listen(process.env.PORT || 5000);
-console.log("Server Started");
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.get('/', function (req, res) {
+    res.send("<h1>AJAY</h1>");
+});
+
+app.listen(port);
+
+
+/*
+ var http = require('http')
+
+ http.createServer(function (req, res) {
+ res.writeHead(200, {'content-type': 'text/html'});
+ res.end("<h1>Hello World</h1>");
+ }).listen(process.env.PORT || 5000);
+ console.log("Server Started");*/
