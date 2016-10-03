52f91bb351d454659b7450479bc32e705fd4cb88
Ajay Arjun <arjun.bka@gmail.com>
2016-10-03 00:30:57 -0500

    external js

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 var express = require('express');
 var pug = require('pug');
 var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 var app = express();
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public',express.static(path.join(__dirname, 'public')));
 
-
-
-app.get('/', function (req, res) {
-    res.send("<h1>AJAY</h1>");
+app.use('/', function (req, res, next) {
+    console.log('Request Type:', req.method,' url: ',req.url);
+    next();
 });
 
-app.get('/a', function (req, res) {
+app.get('/', function (req, res) {
     res.render('hello',{'name':'ajay'});
+});
+
+app.get('/json', function (req, res) {
+    var obj;
+    fs.readFile('sample.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        obj =JSON.parse(data)
+        res.render('apo',{'json':JSON.stringify(obj )})
+        //res.json(obj);
+    });
 });
 
 
 app.listen(port);
+
+
+
---
+++ public/js/aj.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Ajay Arjun on 10/2/2016.
+ */
+function help() {
+    alert("HELLO FROM THE OTHER SIDE");
+}
