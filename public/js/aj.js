14091cb04d1b0f4e15624666e7ed8152f85e2619
Ajay Arjun <arjun.bka@gmail.com>
2016-10-06 12:18:48 -0500

    wrapping my head around angular

--- public/js/aj.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Ajay Arjun on 10/2/2016.
- */
-function help() {
-    alert("HELLO FROM THE OTHER SIDE");
-}
---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Ajay Arjun on 10/2/2016.
+ */
+function help() {
+    alert("HELLO FROM THE OTHER SIDE");
+}
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope, $http) {
+    $scope.click = function () {
+        $scope.sample = ["a","b","c"];
+        $http.get("https://cse5335-axb3015.herokuapp.com/json")
+            .success(function (response) {
+                $scope.mydata = response;
+            });
+    }
+});
