3e3980d187f28be9ea119a620fc4699743c72481
Ajay Arjun <arjun.bka@gmail.com>
2016-10-06 21:53:24 -0500

    wrapping my head around angular

--- public/js/app.js
+++ public/js/app.js
@@ -8,13 +8,11 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     $scope.click = function () {
         $scope.sample = ["a","b","c"];
-        //$http.get("https://cse5335-axb3015.herokuapp.com/json")
-        $http.get("https://localhost:5000/json")
+        $http.get("https://cse5335-axb3015.herokuapp.com/json")
             .success(function (response) {
                 $scope.mydata = response;
             });
         var tab = document.getElementById("table")
         tab.innerHTML='<table><th>Data</th><tr ng-repeat="person in mydata"><td>{{person}}</td></tr></table>';
-
     }
 });
