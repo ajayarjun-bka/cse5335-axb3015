a0ee0b366e2df99090fa4d07a6a16f00f6990af9
Ajay Arjun <arjun.bka@gmail.com>
2016-10-07 02:46:19 -0500

    wrapping my head around angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,15 @@
 /**
  * Created by Ajay Arjun on 10/2/2016.
  */
-
+function help() {
+    alert("HELLO FROM THE OTHER SIDE");
+}
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     $scope.click = function () {
-        $http.get("https://cse5335-axb3015.herokuapp.com/json")
+        $http.get("https://cse5335-axb3015.herokuapp.com/jsn")
             .then(function (response) {
                 $scope.mydata =response;
-            },function(response){
-                alert(response.status)
-                console.log(response.status);
             });
-
         }
 });
