6708755fb2df3323bf992222235e13f2641ecf2d
Ajay Arjun <arjun.bka@gmail.com>
2016-10-14 14:11:41 -0500

    wrapping my head around angular

--- public/js/app.js
+++ public/js/app.js
@@ -8,11 +8,10 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     $scope.click = function () {
         $http.get("https://cse5335-axb3015.herokuapp.com/json")
-            .success(function (response) {
-                $scope.mydata =response;
-            })
-            .error(function (response) {
-                    console.log("Error"+response.status)
-                })
-        }
+            .then((function (response) {
+                $scope.mydata = response;
+            }),(function (response) {
+                console.log("Error" + response.status);
+            }))
+    }
 });
