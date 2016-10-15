947b03d3f116e5ed7aae18d087c0b4eefe68c7a8
Ajay Arjun <arjun.bka@gmail.com>
2016-10-15 17:16:20 -0500

    wrapping my head around angular

--- public/js/app.js
+++ public/js/app.js
@@ -19,28 +19,30 @@ app.controller('myCtrl', function ($scope, $http) {
     }
 });
 
+function loadgraph() {
+    alert('called load')
+    google.charts.load('current', {'packages': ['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    response = null;
+    $.get("https://cse5335-axb3015.herokuapp.com/graphdata", function (data, status) {
+        alert("Data: " + data + "\nStatus: " + status);
+        response = data;
 
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-response=null;
-$.get("https://cse5335-axb3015.herokuapp.com/graphdata", function (data, status) {
-    alert("Data: " + data + "\nStatus: " + status);
-    response = data;
-
-});
-function drawChart() {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    console.log(response)
-    for (r in response) {
-        data.addRow([response[r].label, response[r].count]);
+    });
+    function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        console.log(response)
+        for (r in response) {
+            data.addRow([response[r].label, response[r].count]);
+        }
+        var options = {
+            'title': 'How Much Pizza I Ate Last Night',
+            'width': 500,
+            'height': 300
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
     }
-    var options = {
-        'title': 'How Much Pizza I Ate Last Night',
-        'width': 500,
-        'height': 300
-    };
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
 }
