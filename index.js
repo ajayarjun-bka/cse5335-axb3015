8dc2a916f04e5aaee46196cf714e6687b5209719
Ajay Arjun <arjun.bka@gmail.com>
2016-10-21 17:00:01 -0500

    wrapping my head around angular

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ app.get('/graphdata',function(req,res)
     res.json(rows);
 });
 
+app.get('/mapdata',function(req,res)
+{
+    var data = [{lat:32.733487, lng: -97.120123},{lat:32.735095, lng:-97.114823},{lat:32.735966, long:-97.113788}];
+    //var rows=JSON.parse(data);
+    res.json(data);
+});
+
 app.listen(port);
 
 
